9e0eb6d2517c3afc24e2467298c2c1cd6604963c
Gabriel Aaron <gaaron2@illinois.edu>
2020-10-05 19:35:17 +0200

    added example file for how to get stock info for display

---
+++ stockInfo.py
@@ -0,0 +1,45 @@
+""" This file shows examples of how to get data to display on the webpage"""
+
+# import yfinance library
+import yfinance as yf
+
+tickerName = "AMZN"             # example ticker: GOOG
+ticker = yf.Ticker(tickerName)  # create Ticker object
+info = ticker.info              # get info
+
+
+# RIGHT BANNER
+# company name
+name = info['longName']
+
+# sector
+sector = info['sector']
+
+# current price
+currentPrice = str(info['bid']) + " " + info['currency']
+
+# day's range
+dayRange = str(info['dayLow']) + " - " + str(info['dayHigh']) + " USD"
+
+# 52 week range
+yrRange = str(info['fiftyTwoWeekLow']) + " - " + str(info['fiftyTwoWeekHigh']) + " USD"
+
+# average volume
+avgVolume = str(info['averageVolume'])
+
+# trailing EPS (uses historical data)
+epsTrail = str(info['trailingEps'])
+
+# forward EPS (uses forecasted data)
+epsFwd = str(info['forwardEps'])
+
+# market capital
+marketCap = str(info['marketCap'])
+
+
+# PRICE GRAPH
+# get historical data. available periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max. default = 1mo
+historical = ticker.history(period='max')
+
+# get closing prices from historical data
+closePrice = historical.Close
