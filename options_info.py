780b29293ecfab4d6a21c67388d8289165a10543
Eshaan <ebhattad@gmail.com>
2020-10-09 11:24:45 -0500

    Added Documentation to module

--- options_info.py
+++ options_info.py
@@ -3,24 +3,29 @@ import pandas as pd
 import re
 
 
+# Returns Ticker object from Yahoo Finance when given a ticker
 def getStock(ticker):
     return yf.Ticker(ticker)
 
 
+# Returns all the possible expiration dates of a ticker
 def getExpirationDates(ticker):
     stock = yf.Ticker(ticker)
     expirations = stock.options
     return expirations
 
 
+# Returns the call options at a certain expiration date and ticker
 def getCalls(ticker, date):
     return yf.Ticker(ticker).option_chain(date).calls
 
 
+# Returns the puts options at a certain expiration date and ticker
 def getPuts(ticker, date):
     return yf.Ticker(ticker).option_chain(date).puts
 
 
+# Returns the data that would be presented in a table to the user
 def findRelevantData(optionChain):
     data = optionChain[['contractSymbol', 'strike', 'impliedVolatility', 'lastPrice']]
     additionalData = pd.DataFrame({"expirationDate": [],
@@ -29,20 +34,21 @@ def findRelevantData(optionChain):
     for contract in optionChain['contractSymbol']:
         expirationDate = '20'
         information = parseContractSymbol(str(contract))
-        typeOfOption = information.charAt(len(information)-1)
-        print(typeOfOption)
         for x in range(len(information)):
-            if x<2:
+            if x < 2:
                 expirationDate = expirationDate
 
 
+# Parses the contract symbol down to the form YYMMDD and option type
 def parseContractSymbol(contract):
     if (first_digit := re.search(r"\d", contract)) is not None:
         contract = contract[first_digit.start()::]
     index = contract.index('C')
-    contract = contract[0:index+1]
+    contract = contract[0:index + 1]
     return contract
 
+
+#Testing
 print(getExpirationDates(getStock('BRK-B').ticker))
 
 print(findRelevantData(getCalls('AAPL', '2020-10-09')))
