1c9ba1298502a5b2a06f13d2d203fb46345cb811
Gabriel Aaron <gaaron2@illinois.edu>
2020-10-10 18:35:26 +0200

    Tweaked candlestick chart

--- candlestick.py
+++ candlestick.py
@@ -4,15 +4,23 @@ from datetime import datetime
 
 from stock_info import StockInfo
 
-google = StockInfo("GOOG")
-googlehist = google.get_hist("6mo")
-print(googlehist)
+ticker = "GOOG"
+
+stock = StockInfo(ticker)
+hist = stock.get_hist("max")
+print(hist)
 
 fig = go.Figure(data=[go.Candlestick(
-    x=      googlehist['Date'],
-    open=   googlehist['Open'],
-    high=   googlehist['High'],
-    low=    googlehist['Low'],
-    close=  googlehist['Close'])])
+    x=      hist['Date'],
+    open=   hist['Open'],
+    high=   hist['High'],
+    low=    hist['Low'],
+    close=  hist['Close'])])
+
+fig.update_layout(
+    title=stock.name + ' (' + ticker + ')',
+    yaxis_title='Stock Price (USD)',
+    xaxis_rangeslider_visible=False
+)
 
 fig.show()
