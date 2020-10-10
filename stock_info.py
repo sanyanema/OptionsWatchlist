23968e476586c2f383136c6a07163c099599e439
Gabriel Aaron <gaaron2@illinois.edu>
2020-10-10 16:12:13 +0200

    Defined StockInfo class to facilitate getting relevant information and close price history.

--- stock_info.py
+++ stock_info.py
@@ -1,50 +1,74 @@
-""" This file shows examples of how to get data to display on the webpage"""
+""" This file defines the class StockInfo.
+
+    A new instance of StockInfo contains the necessary info about the stock to be
+    displayed on the visualization page.
+
+    The instance method getHist returns the close price history for the desired period.
+ """
 
 # import yfinance library
 import yfinance as yf
 
-tickerName = "GOOG"             # example ticker: GOOG
-ticker = yf.Ticker(tickerName)  # create Ticker object
-info = ticker.info              # get info
+class StockInfo:
+    def __init__(self, tickerName):
+        """
+        class creates object containing relevant info for desired stock
+        :param tickerName: [string] stock ticker
+        """
+        # create Ticker object
+        self.ticker = yf.Ticker(tickerName)
 
+        # get info
+        info = self.ticker.info
 
-# RIGHT BANNER
-# company name
-name = info['longName']
+        # company name
+        self.name = info['longName']
 
-# sector
-sector = info['sector']
+        # sector
+        self.sector = info['sector']
 
-# current price
-currentPrice = str(info['bid']) + " " + info['currency']
+        # current price
+        self.current_price = str(info['bid']) + " " + info['currency']
 
-# day's range
-dayRange = str(info['dayLow']) + " - " + str(info['dayHigh']) + " USD"
+        # day's range
+        self.day_range = str(info['dayLow']) + " - " + str(info['dayHigh']) + " USD"
 
-# 52 week range
-yrRange = str(info['fiftyTwoWeekLow']) + " - " + str(info['fiftyTwoWeekHigh']) + " USD"
+        # 52 week range
+        self.yr_range = str(info['fiftyTwoWeekLow']) + " - " + str(info['fiftyTwoWeekHigh']) + " USD"
 
-# average volume
-avgVolume = str(info['averageVolume'])
+        # average volume
+        self.avg_volume = str(info['averageVolume'])
 
-# trailing EPS (uses historical data)
-epsTrail = str(info['trailingEps'])
+        # trailing EPS (uses historical data)
+        self.eps_trail = str(info['trailingEps'])
 
-# forward EPS (uses forecasted data)
-epsFwd = str(info['forwardEps'])
+        # forward EPS (uses forecasted data)
+        self.eps_fwd = str(info['forwardEps'])
 
-# market capital
-marketCap = str(info['marketCap'])
+        # market capital
+        self.market_cap = str(info['marketCap'])
 
+    def getHist(self,period):
+        """ function retrieves close price history for desired stock
 
-# PRICE GRAPH
-# get historical data. available periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max. default = 1mo
-historical = ticker.history(period='max')
+        :param period: [string] desired graphing period (1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max)
+        :return: [pandas dataframe] dataframe of historical closing prices
+        """
 
-# get closing prices from historical data
-closePrice = historical.Close
+        # check if period is valid
+        periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+        if period not in periods:
+            return "chosen period is not available"
 
-def getTickerInfo(tickerName) :
-    ticker = yf.Ticker(tickerName)  # create Ticker object
-    return ticker.info
+        # get historical data. available periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max. default = 1mo
+        historical = self.ticker.history(period=period)
 
+        # get closing prices from historical data
+        close_price = historical.Close
+
+        return close_price
+
+# example code
+google = StockInfo("goog")
+print(google.sector)
+print(google.getHist("6mo"))
--- yfinance_test.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-
-goog = yf.Ticker("GOOG")
-
-option = goog.history(period='max')
-
-print(option)
