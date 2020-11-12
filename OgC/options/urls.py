4d078c2ba285817908550e20b8bffc0b24310c96
josephkuang <josephjkuang@gmail.com>
2020-11-12 16:15:30 -0600

    Delted everything

--- OgC/options/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SearchResultsView, HomepageView
-
-urlpatterns = [
-    path('search/',SearchResultsView.as_view(), name='search_results'),
-    path('',HomepageView.as_view(), name='home')
-]
