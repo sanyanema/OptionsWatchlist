05a4eb66eeda1d2c9bce6f91ea271e4e94ac0a13
eshaan-bhattad <eshaan2@illinois.edu>
2020-12-07 00:51:50 -0600

    Clearing master

--- OgC/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import WatchList, Item
-# Register your models here.
-
-admin.site.register(WatchList)
--- OgC/options/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Option 
-# Register your models here.
-class OptionAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-admin.site.register(Option, OptionAdmin)
